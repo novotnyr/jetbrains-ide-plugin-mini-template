14863ded757533b57dcba65696dbe5468c2e188b
Robert Novotny <robert.novotny@jetbrains.com>
2026-03-25 13:22:30 +0100

    Setup buildscript for plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.20"
+    id("org.jetbrains.intellij.platform") version "2.13.1"
 }
 
 group = "com.github.novotnyr"
@@ -7,16 +8,34 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    intellijPlatform {
+        intellijIdea("2024.1")
+    }
 }
 
 kotlin {
     jvmToolchain(21)
 }
 
-tasks.test {
-    useJUnitPlatform()
+intellijPlatform {
+    pluginConfiguration {
+        name = "Minimal Plugin Template"
+        version = project.version.toString()
+        description = "Minimal plugin template with barebones Gradle setup"
+        ideaVersion {
+            sinceBuild = "2024.1"
+        }
+    }
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
 }
