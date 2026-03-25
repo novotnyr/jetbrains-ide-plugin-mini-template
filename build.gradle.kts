9870c8c043412515c91aff1939fed3e536912bc1
Robert Novotny <robert.novotny@jetbrains.com>
2026-03-25 21:55:02 +0100

    Move Kotlin toolchain to allprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,12 +34,12 @@ intellijPlatform {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.intellij.platform.module")
-    kotlin {
-        jvmToolchain(21)
-    }
 }
 
 allprojects {
+    kotlin {
+        jvmToolchain(21)
+    }
     dependencies {
         intellijPlatform {
             intellijIdea("2025.3")
