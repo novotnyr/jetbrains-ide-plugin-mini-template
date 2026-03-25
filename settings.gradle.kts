cc4b8386beccf57353bf79ec9b3bbc03387698b8
Robert Novotny <robert.novotny@jetbrains.com>
2026-03-25 21:03:24 +0100

    Change root project name as this impact content module name prefixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
-rootProject.name = "jetbrains-ide-plugin-mini-template"
+rootProject.name = "modular-plugin-mini-template"
 include("shared")
 include("css")
 
